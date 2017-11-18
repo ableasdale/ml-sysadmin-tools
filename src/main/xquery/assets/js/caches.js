d28fe1a18c80f44434fe8d76fbae74c13fb9ec00
Alex Bleasdale <ableasdale@marklogic.com>
2017-11-18 12:59:00 +0000

    cache work complete (for single node representation)

--- src/main/xquery/assets/js/caches.js
+++ src/main/xquery/assets/js/caches.js
@@ -18,15 +18,19 @@ function generateOptions(subtitle) {
             }
         }
     }
-};
+}
 
 function genData(objs, mode) {
     var cacheItems = [];
 
-    if (mode === 6){
+    if (mode === 7) {
+        cacheItems.push(['Partition Size (MB)', 'Partition Busy', 'Partition Used', 'Partition Free','Value Count', 'Value Bytes Total', 'Value Bytes Average']);
+    } else if (mode === 6) {
         cacheItems.push(['Partition Size (MB)', 'Partition Table', 'Partition Busy','Partition Used', 'Partition Free', 'Partition Overhead']);
-    } else {
+    } else if (mode === 5) {
         cacheItems.push(['Partition Size (MB)', 'Partition Table', 'Partition Used', 'Partition Free', 'Partition Overhead']);
+    } else {
+        cacheItems.push(['Partition Size (MB)', 'Partition Busy', 'Partition Used', 'Partition Free']);
     }
 
     for (var index in objs) {
@@ -34,23 +38,31 @@ function genData(objs, mode) {
         cacheItems.push(tagsArray);
     }
     return cacheItems;
-};
+}
 
 function drawChart() {
-    var tc = document.getElementById('data').textContent;
+    var tc = document.getElementById('cache-data').textContent;
     var objsCTC = JSON.parse(tc).compressedTreeCachePartitions.map(o => Object.values(o));
     var objsETC = JSON.parse(tc).expandedTreeCachePartitions.map(o => Object.values(o));
     var objsLC = JSON.parse(tc).expandedTreeCachePartitions.map(o => Object.values(o));
+    var objsTC = JSON.parse(tc).tripleCache.tripleCachePartitions.map(o => Object.values(o));
+    var objsTVC = JSON.parse(tc).tripleValueCache.tripleValueCachePartitions.map(o => Object.values(o));
 
-    var dataCTC = google.visualization.arrayToDataTable(genData(objsCTC, 0));
+    var dataCTC = google.visualization.arrayToDataTable(genData(objsCTC, 5));
     var dataETC = google.visualization.arrayToDataTable(genData(objsETC, 6));
     var dataLC = google.visualization.arrayToDataTable(genData(objsLC, 6));
+    var dataTC = google.visualization.arrayToDataTable(genData(objsTC, 0));
+    var dataTVC = google.visualization.arrayToDataTable(genData(objsTVC, 7));
 
     var compressedTreeChart = new google.charts.Bar(document.getElementById('ctc'));
     var expandedTreeChart = new google.charts.Bar(document.getElementById('etc'));
     var listCacheChart = new google.charts.Bar(document.getElementById('lc'));
+    var tripleCacheChart = new google.charts.Bar(document.getElementById('tc'));
+    var tripleValueCacheChart = new google.charts.Bar(document.getElementById('tvc'));
 
-    compressedTreeChart.draw(dataCTC, generateOptions("Compressed Tree Cache"));
-    expandedTreeChart.draw(dataETC, generateOptions("Expanded Tree Cache"));
-    listCacheChart.draw(dataLC, generateOptions("List Cache"));
-};
+    compressedTreeChart.draw(dataCTC, generateOptions("Compressed Tree Cache ["+objsCTC.length+" partition(s)]"));
+    expandedTreeChart.draw(dataETC, generateOptions("Expanded Tree Cache ["+objsETC.length+" partition(s)]"));
+    listCacheChart.draw(dataLC, generateOptions("List Cache ["+objsLC.length+" partition(s)]"));
+    tripleCacheChart.draw(dataTC, generateOptions("Triple Cache ["+objsTC.length+" partition(s)]"));
+    tripleValueCacheChart.draw(dataTVC, generateOptions("Triple Value Cache ["+objsTVC.length+" partition(s)]"));
+}
--- src/main/xquery/cache-status.xqy
+++ src/main/xquery/cache-status.xqy
@@ -6,7 +6,7 @@ declare namespace cs = "http://marklogic.com/xdmp/status/cache";
 import module namespace lib-view = "http://www.marklogic.com/sysadmin/lib-view" at "lib/lib-view.xqy";
 
 (: TODO - parameterise debug :)
-declare variable $debug := fn:true();
+declare variable $debug := xdmp:get-request-field("debug", "0");
 declare variable $cache-status := xdmp:cache-status();
 
 (: Module main :)
@@ -16,10 +16,12 @@ attribute class {"container"},
 lib-view:page-header("MarkLogic Cluster", "Group Level Caches", " "),
 element div {attribute class {"row"},
 element h4 {xs:string($cache-status/cs:host-name) || " ", element small {xs:string($cache-status/cs:host-id)}},
-element div {attribute id {"data"}, text{ xdmp:javascript-eval("xdmp.cacheStatus(xdmp.host())")}},
+element div {attribute id {"cache-data"}, text{ xdmp:javascript-eval("xdmp.cacheStatus(xdmp.host())")}},
 <div id="ctc" style="width: 900px; height: 500px;">{" "}</div>,
 <div id="etc" style="width: 900px; height: 500px;">{" "}</div>,
 <div id="lc" style="width: 900px; height: 500px;">{" "}</div>,
-if($debug) then(element h3 {"Group Level Cache Status"}, element pre {element code {xdmp:quote(xdmp:cache-status())}}) else ()
+<div id="tc" style="width: 900px; height: 500px;">{" "}</div>,
+<div id="tvc" style="width: 900px; height: 500px;">{" "}</div>,
+if($debug eq "1") then(element h3 {"Group Level Cache Status (Debug Mode)"}, element pre {element code {xdmp:quote(xdmp:cache-status())}}) else ()
 }
 },<script src="/assets/js/caches.js">{" "}</script>)
