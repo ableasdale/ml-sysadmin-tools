d3588766102c8bff3bdaa4bd101219a7add43233
Alex Bleasdale <ableasdale@marklogic.com>
2017-11-18 12:28:22 +0000

    actually generates real cache data now! :)

--- src/main/xquery/assets/js/caches.js
+++ src/main/xquery/assets/js/caches.js
@@ -1,7 +1,5 @@
-// google.charts.load("current", { packages: ["corechart"] });
-
 google.charts.load("current", {
-    packages: ["bar"]
+    packages: ["corechart", "bar"]
 });
 
 google.charts.setOnLoadCallback(drawChart);
@@ -22,22 +20,37 @@ function generateOptions(subtitle) {
     }
 };
 
+function genData(objs, mode) {
+    var cacheItems = [];
+
+    if (mode === 6){
+        cacheItems.push(['Partition Size (MB)', 'Partition Table', 'Partition Busy','Partition Used', 'Partition Free', 'Partition Overhead']);
+    } else {
+        cacheItems.push(['Partition Size (MB)', 'Partition Table', 'Partition Used', 'Partition Free', 'Partition Overhead']);
+    }
+
+    for (var index in objs) {
+        var tagsArray = objs[index];
+        cacheItems.push(tagsArray);
+    }
+    return cacheItems;
+};
+
 function drawChart() {
-    var data = google.visualization.arrayToDataTable([
-        ['Partition Size (MB)', 'Partition Table', 'Partition Busy', 'Partition Used', 'Partition Free', 'Partition Overhead'],
-        ['512', 1.6, 0, 93.7, 6.3, 0],
-        ['256', 1.6, 0, 43.7, 6.3, 0]
-    ]);
+    var tc = document.getElementById('data').textContent;
+    var objsCTC = JSON.parse(tc).compressedTreeCachePartitions.map(o => Object.values(o));
+    var objsETC = JSON.parse(tc).expandedTreeCachePartitions.map(o => Object.values(o));
+    var objsLC = JSON.parse(tc).expandedTreeCachePartitions.map(o => Object.values(o));
 
-    //is3D: true,
+    var dataCTC = google.visualization.arrayToDataTable(genData(objsCTC, 0));
+    var dataETC = google.visualization.arrayToDataTable(genData(objsETC, 6));
+    var dataLC = google.visualization.arrayToDataTable(genData(objsLC, 6));
 
-
-    //var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
     var compressedTreeChart = new google.charts.Bar(document.getElementById('ctc'));
     var expandedTreeChart = new google.charts.Bar(document.getElementById('etc'));
     var listCacheChart = new google.charts.Bar(document.getElementById('lc'));
 
-    compressedTreeChart.draw(data, generateOptions("Compressed Tree Cache"));
-    expandedTreeChart.draw(data, generateOptions("Expanded Tree Cache"));
-    listCacheChart.draw(data, generateOptions("List Cache"));
+    compressedTreeChart.draw(dataCTC, generateOptions("Compressed Tree Cache"));
+    expandedTreeChart.draw(dataETC, generateOptions("Expanded Tree Cache"));
+    listCacheChart.draw(dataLC, generateOptions("List Cache"));
 };
